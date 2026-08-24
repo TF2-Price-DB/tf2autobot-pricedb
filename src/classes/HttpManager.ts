e9a722fca73d5f65977b589598c9fa7f0f6d8836
Oliver Perring <32413830+OliverPerring@users.noreply.github.com>
2026-08-24 08:37:57 +0100

    docs(http): describe trade API at startup

--- src/classes/HttpManager.ts
+++ src/classes/HttpManager.ts
@@ -480,9 +480,8 @@ export default class HttpManager {
         return new Promise(resolve => {
             this.app.listen(this.options.httpApiPort, () => {
                 log.debug(`HTTP Server started: http://127.0.0.1:${this.options.httpApiPort}`);
-                log.info(`This is NOT a HTTP API used to handle data within the bot.
-                It is solely for managing the bot programatically by providing healthchecks & uptime details.`);
-                log.info(`Please use the TF2Bot GUI v3+ as the main API source.`);
+                log.info(`HTTP API provides health checks, uptime details, and authenticated trade offer management.`);
+                log.info(`For the full bot-management interface, use TF2Bot GUI v3+.`);
                 log.info(`https://github.com/TF2Autobot/tf2autobot-gui`);
                 resolve();
             });
